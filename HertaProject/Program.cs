17c10ede04e5822fbd7dd25808288ba3f518b2c3
Daniel Renato Marlen <sherlockdaniel4869@mal.comgi>
2024-09-21 14:58:50 +0700

    last update

--- HertaProject/Program.cs
+++ HertaProject/Program.cs
@@ -13,6 +13,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddHttpClient();
 
 builder.Services.AddDbContext<ApplicationDbContext>(options=>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
